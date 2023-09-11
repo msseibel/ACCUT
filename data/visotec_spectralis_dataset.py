abef17765cb600017ffbade59fc763a4093f1fad
Marc Steffen Seibel <msseibel@users.noreply.github.com>
2023-09-11 14:57:27 +0200

    not working - just code checkpoint

--- data/visotec_spectralis_dataset.py
+++ data/visotec_spectralis_dataset.py
@@ -1,5 +1,5 @@
 import os
-from data.base_dataset import BaseDataset, get_transform
+from data.base_dataset import BaseDataset, get_transform, get_transforms_dict
 from data.image_folder import make_dataset
 from PIL import Image
 import random
@@ -13,20 +13,37 @@ Image.MAX_IMAGE_PIXELS = None  # Disable DecompressionBombError
 ImageFile.LOAD_TRUNCATED_IMAGES = True  # Disable OSError: image file is truncated
 
 
+def parse_directory(directory, startswith):
+    files = {'Spectralis':[], 'Visotec':[]}
+    device_counter = {'Spectralis':0, 'Visotec':0}
+    
+    if directory.split('/')[-1] in ['Spectralis', 'Visotec']:
+        device = directory.split('/')[-1]
+        for sub in os.listdir(directory):
+            for img in os.listdir(os.path.join(directory, sub)):
+                if img.startswith(startswith):
+                    path = os.path.join(directory, sub, img)
+                    device_counter[device] += 1
+                    files[device].append(path)
+
+    else: #slower method
+        for root, _, fnames in tqdm.tqdm(sorted(os.walk(directory))):
+            for fname in fnames:
+                if fname.startswith(startswith) and fname.endswith('.tiff'): # 
+                    path = os.path.join(root, fname)
+                    for device in device_counter.keys():
+                        if device in path.split('/'):
+                            device_counter[device] += 1
+                            files[device].append(path)
+    return files, device_counter
+
 # get recursively all tiff files which start with X under a directory
 def get_tiff_files(directory, startswith, min_samples=None):
     print('Start loading files...', directory)
-    files = {'Spectralis':[], 'Visotec':[]}
-    device_counter = {'Spectralis':0, 'Visotec':0}
-    for root, _, fnames in tqdm.tqdm(sorted(os.walk(directory))):
-        for fname in fnames:
-            if fname.startswith(startswith) and fname.endswith('.tiff'):
-                path = os.path.join(root, fname)
-                for device in device_counter.keys():
-                    if device in path.split('/'):
-                        device_counter[device] += 1
+    #files = {'Spectralis':[], 'Visotec':[]}
+    #device_counter = {'Spectralis':0, 'Visotec':0}
+    files, device_counter = parse_directory(directory, startswith)
 
-                        files[device].append(path)
     if min_samples=='balanced':
         rng = np.random.RandomState(0)
         common_min = min(device_counter.values())
@@ -49,25 +66,21 @@ class VisotecSpectralisDataset(BaseDataset):
         BaseDataset.__init__(self, opt)
         self.dir_A = opt.content_path
         self.dir_B = opt.style_path
-        if opt.mixed:
-            files = get_tiff_files(self.dir_A,'X', 'balanced')
-            files = files['Spectralis'].tolist() + files['Visotec'].tolist()
-            self.A_paths = files.copy()#sorted(make_dataset(self.dir_A, opt.max_dataset_size))
-            self.B_paths = files.copy()#sorted(get_tiff_files(self.dir_B,'X'))#sorted(make_dataset(self.dir_B, opt.max_dataset_size))
-        else:
-            filesA = get_tiff_files(self.dir_A,'X', None)
-            filesB = get_tiff_files(self.dir_B,'X', None)
-            self.A_paths = np.concatenate(list(filesA.values()))#.copy()#sorted(make_dataset(self.dir_A, opt.max_dataset_size))
-            self.B_paths = np.concatenate(list(filesB.values()))#.copy()#sorted(get_tiff_files(self.dir_B,'X'))#sorted(make_dataset(self.dir_B, opt.max_dataset_size))
-            
+
+        filesA = get_tiff_files(self.dir_A,'X', None)
+        filesB = get_tiff_files(self.dir_B,'X', None)
+        self.A_paths = np.concatenate(list(filesA.values()))#.copy()#sorted(make_dataset(self.dir_A, opt.max_dataset_size))
+        self.B_paths = np.concatenate(list(filesB.values()))#.copy()#sorted(get_tiff_files(self.dir_B,'X'))#sorted(make_dataset(self.dir_B, opt.max_dataset_size))
+        print('Num Files A: ', len(self.A_paths))
+        print('Num Files B: ', len(self.B_paths))
         
         self.A_size = len(self.A_paths)
         self.B_size = len(self.B_paths)
         print(self.opt)
         print('crop_pos' in self.opt)
-
-        self.transform_A = get_transform(self.opt)
-        self.transform_B = get_transform(self.opt)
+        
+        self.transform_A = get_transforms_dict(dict(dataset=self.dir_A.split('/')[-1].lower()))#get_transform(self.opt)
+        self.transform_B = get_transforms_dict(dict(dataset=self.dir_B.split('/')[-1].lower()))#get_transform(self.opt)
 
     def __getitem__(self, index):
         if self.opt.isTrain:
@@ -79,19 +92,34 @@ class VisotecSpectralisDataset(BaseDataset):
 
 
         A_path = self.A_paths[index_A]
+        A_path_mask = A_path.replace('/X', '/Y')
         A_img = Image.open(A_path)
+        A_mask = Image.open(A_path_mask)
         A_img = Image.fromarray(normalize(np.array(A_img))*255)
-        A_img = A_img.convert('RGB')
-        A = self.transform_A(A_img)
 
 
         B_path = self.B_paths[index_B]
         B_img = Image.open(B_path)
-        B_img = Image.fromarray(normalize(np.array(B_img))*255)
-        B_img = B_img.convert('RGB')
-        B = self.transform_B(B_img)
+        if self.dir_B.split('/')[-1] == 'Visotec':
+            B_img = np.array(B_img)
+            q05q995 = np.percentile(B_img, [0.5, 99.5])
+            B_img = np.clip(B_img, q05q995[0], q05q995[1])
+            B_img = Image.fromarray(normalize(B_img)*255)
+        else:
+            B_img = Image.fromarray(normalize(np.array(B_img))*255)
+        
+        if self.opt.input_nc == 3:
+            A_img = A_img.convert('RGB')
+            B_img = B_img.convert('RGB')
 
+        A = self.transform_A(A_img) 
+        A -= A.min()
+        B = self.transform_B(B_img) 
+        B -= B.min()
+        A /= A.max()
+        B /= B.max()
 
+        mask_B = None
 
 
         name_A = '_'.join(A_path.split('/')[-3:])#os.path.basename(A_path)
@@ -100,7 +128,12 @@ class VisotecSpectralisDataset(BaseDataset):
             name = name_B[:name_B.rfind('.')] + '_' + name_A[:name_A.rfind('.')] + name_A[name_A.rfind('.'):]
         else:
             name = name_A[:name_A.rfind('.')] + name_A[name_A.rfind('.'):]
-        result = {'A': A, 'B': B, 'name': name, 'style_name':name_B, 'ori_size':A_img.size, 'A_paths': A_path, 'B_paths': B_path}
+        result = {'A': A, 'B': B, 
+                  'mask_A': mask_A, 
+                  'mask_B': mask_B,
+                  'name': name, 'style_name':name_B, 
+                  'ori_size':A_img.size, 
+                  'A_paths': A_path, 'B_paths': B_path}
         return result
 
     def __len__(self):
