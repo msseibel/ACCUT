680a9304ed122233793cd6a26005183b41cbfd7b
Marc Seibel <marc.seibel@uni-luebeck.de>
2023-11-13 10:43:07 +0000

    start abltation testing

---
+++ start_test_s2v_ablation.py
@@ -0,0 +1,40 @@
+import subprocess
+src = 'Spectralis'
+tgt = 'Visotec'
+version =14.1
+
+#output_dir = 'bvm_baseline'
+#output_dir = 'bvm_doubleseg'
+#output_dir = 'bvm_targetseg'
+#output_dir = 'bvm_sourceseg'
+
+mode = 'target'
+output_dir = f'bvm_ablationpermute_{mode}seg'
+
+if output_dir == 'bvm_baseline':
+    lambda_seg_src = '0.0'
+else:
+    lambda_seg_src = '1.0'
+epoch = 250
+#../../tmp/Spectralis_Visotec_v14.1/tiff/Spectralis
+subprocess.run(['python', 'test.py',
+"--content_path", f"../../tmp/Spectralis_Visotec_v{version}/tiff/{src}",
+"--style_path", f"../../tmp/Spectralis_Visotec_v{version}/tiff/{tgt}" ,
+#"--content",f"{src}",
+#"--style", f"{tgt}",
+"--name", f'spec2home_CUT_bvm_{mode}seg_lsgan',
+"--model", 'semcutablation',
+"--CUT_mode", "CUT",
+"--dataset_mode", 'visotec_spectralis_ablation',
+"--load_size", '512',
+"--crop_size", '240',
+"--gpu_ids", '0',
+'--num_test', '10000000',
+"--output_dir", f'{output_dir}',
+"--input_nc", "1",
+"--output_nc", "1",
+"--use_seg_src", "True",
+"--use_seg_tgt", "True",
+"--epoch", f"{epoch}",
+"--lambda_seg_src", lambda_seg_src,
+])
