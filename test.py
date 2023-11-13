98e623a85138d0a2745b00279710c125c84c1348
Marc Seibel <marc.seibel@uni-luebeck.de>
2023-11-13 10:41:44 +0000

    test save to tiff

--- test.py
+++ test.py
@@ -33,7 +33,88 @@ from models import create_model
 from util.visualizer import save_images
 from util import html
 import util.util as util
+from PIL import Image
+import numpy as np
+from pathlib import Path
+import csv
 
+def append_row_to_csv(row, csv_file_path):    
+    with open(csv_file_path, 'a') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow(row)
+
+def get_eye(img_name):
+    if 'OD' in img_name:
+        return 'OD'
+    elif 'OS' in img_name:
+        return 'OS'
+    else:
+        return 'XX'
+    
+def info_from_img_name(img_name):
+    # format of name: domain_subid_eye-001.png
+    domain = img_name.split('_')[0]
+    sub_id = img_name.split('_')[1]
+    eye    = get_eye(img_name)
+    slice_idx = img_name.split('.')[0].split('-')[-1]
+    assert len(slice_idx) == 3, f"slice_idx: {slice_idx}, img_name: {img_name}" # We assume that the slice index is 3 digits long (padded with zeros)
+    assert domain in ['RETOUCH-Cirrus2Topcon', 
+                      'RETOUCH-Cirrus2Spectralis',
+                      'RETOUCH-Topcon2Cirrus', 
+                      'RETOUCH-Topcon2Spectralis', 
+                      'RETOUCH-Spectralis2Cirrus', 
+                      'RETOUCH-Spectralis2Topcon', 
+                      'Visotec', 'Spectralis']
+    return domain, sub_id, eye, slice_idx
+    
+def writeX_to_tiff(X_slice, domain, sub_id, eye, slice_idx, output_dir):
+    tmp_dir = Path("../../tmp")
+    os.makedirs(tmp_dir / output_dir / "tiff" / f'{domain}' / f'{sub_id}', exist_ok=True)
+    #print('Save to', tmp_dir / output_dir / "tiff" / f'{domain}' / f'{sub_id}' / f"X-{eye}-{slice_idx}.tiff")
+    if type(X_slice) == np.ndarray:
+        X_slice = Image.fromarray(X_slice)
+ 
+    if domain not in ['RETOUCH-Cirrus2Topcon', 
+                      'RETOUCH-Cirrus2Spectralis',
+                      'RETOUCH-Topcon2Cirrus', 
+                      'RETOUCH-Topcon2Spectralis', 
+                      'RETOUCH-Spectralis2Cirrus', 
+                      'RETOUCH-Spectralis2Topcon', 
+                      'Visotec', 'Spectralis']:
+        raise ValueError(f'Unknown domain {domain}')
+    
+    X_slice.save(tmp_dir / output_dir / "tiff" / f'{domain}' / f'{sub_id}' / f"X-{eye}-{slice_idx}.tiff")
+
+def normalize_volumes(output_dir, domain):
+    tmp_dir = Path("../../tmp")
+    dataset_dir = tmp_dir / output_dir / "tiff" / f'{domain}'
+    for sub_id in os.listdir(dataset_dir):
+        for eye in ['OD', 'OS', 'XX']:
+            # list all files which contain the eye and the sub_id
+            files = [f for f in os.listdir(dataset_dir / sub_id) if eye in f]
+            if len(files) == 0:
+                continue
+            # load all slices of the subject and the eye
+            slices = []
+            slice_idxs = []
+            for f in files:
+                slices.append(np.array(Image.open(dataset_dir / sub_id / f)))
+                slice_idx = f.split('-')[-1].split('.')[0]
+                slice_idxs.append(slice_idx)
+                
+            # normalize the slices
+            slices = np.array(slices)
+            slices = (slices - np.mean(slices)) / np.std(slices)
+            # save the normalized slices
+            for idx in range(len(slices)):
+                writeX_to_tiff(slices[idx], domain, sub_id, eye, slice_idxs[idx], output_dir)
+
+def resize(X, ori_size_wh):
+    X_slice = Image.fromarray(X)
+    # resize back to original size
+    if np.any(X_slice.size!=ori_size_wh):
+        X_slice = X_slice.resize(ori_size_wh, Image.BICUBIC)
+    return X_slice
 
 if __name__ == '__main__':
     opt = TestOptions().parse()  # get test options
@@ -43,13 +124,21 @@ if __name__ == '__main__':
     opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
     opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
     opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
+    output_dir = opt.output_dir
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
-    train_dataset = create_dataset(util.copyconf(opt, phase="train"))
+    #train_dataset = create_dataset(util.copyconf(opt, phase="train"))
     model = create_model(opt)      # create a model given opt.model and other options
     # create a webpage for viewing the results
-    web_dir = os.path.join(opt.results_dir, opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
+    
+    web_dir = Path("../../tmp") / os.path.join(output_dir, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
     print('creating web directory', web_dir)
     webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.epoch))
+
+    csv_file_path = os.path.join(web_dir, f'{opt.name}_mapping.csv')
+    print('Csv file ', csv_file_path)
+    with open(csv_file_path, 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow(['src_img_path', 'style_img_path', 'ablation_img_path'])
 
     for i, data in enumerate(dataset):
         if i == 0:
@@ -63,8 +152,24 @@ if __name__ == '__main__':
         model.set_input(data)  # unpack data from data loader
         model.test()           # run inference
         visuals = model.get_current_visuals()  # get image results
-        img_path = model.get_image_paths()     # get image paths
-        if i % 5 == 0:  # save images to an HTML file
-            print('processing (%04d)-th image... %s' % (i, img_path))
-        save_images(webpage, visuals, img_path, width=opt.display_winsize)
-    webpage.save()  # save the HTML
+        row = []
+        img_path   = model.get_image_paths()     # get image paths
+        style_path = model.get_style_paths()     # get image paths
+        row+=[img_path[0]]
+        row+=[style_path[0]]
+        if hasattr(model, 'get_ablation_paths'):
+            ablation_img_path = model.get_ablation_paths()
+            row+=[ablation_img_path[0]]
+        #print(row)
+        append_row_to_csv(row, csv_file_path=csv_file_path)
+
+        #if i % 5 == 0:  # save images to an HTML file
+        #    print('processing (%04d)-th image... %s' % (i, img_path))
+        #save_images(webpage, visuals, img_path, width=opt.display_winsize)
+        domain, sub_id, eye, slice_idx = info_from_img_name(img_path[0])
+        X = model.fake_B[0,0].detach().cpu().numpy()
+        ori_size_wh = tuple(np.concatenate(data['ori_size']))
+        X_slice = resize(X, ori_size_wh)
+        writeX_to_tiff(X_slice, domain, sub_id, eye, slice_idx, output_dir)
+    normalize_volumes(output_dir, domain)
+    #webpage.save()  # save the HTML
